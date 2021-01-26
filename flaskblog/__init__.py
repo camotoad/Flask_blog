64e6a55a026e95d19b1ebcdc6e028f041bc2f768
GXY <camotoad1@gmail.com>
2021-01-26 13:01:13 -0500

    login, logout

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SK_FlaskBlog')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fb.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'danger'
 
 from flaskblog import routes
